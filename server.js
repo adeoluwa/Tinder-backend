80b47adabb09cabf92eb01e2af30a7dca620068e
Deolu_Josef <kiishijoseph@gmail.com>
2023-05-27 15:50:35 +0100

    installed cors, tested the endpoint

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ import express from 'express';
 
 import mongoose, { Mongoose } from 'mongoose';
 
+import Cors from 'cors';
+
 import dotenv from 'dotenv';
+
+import Cards from './dbCards.js';
 
 // dotenv.config()
 
@@ -12,10 +16,10 @@ mongoose.set('strictQuery', false);
 const app = express();
 
 //Middleware
+app.use(express.json());
+app.use(Cors());
 
 // DB config
-// const PORT = process.env.PORT || 6001;
-
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
@@ -34,8 +38,34 @@ const mongoConnect = async () => {
 };
 
 mongoConnect();
+//
 
 //API Endpoints
+
 app.get('/', (req, res) => res.status(200).send('It is Working'));
 
-// Listener
+
+
+app.post('/tinder/cards', (req, res) => {
+  const dbCard = req.body;
+  Cards.create().then((error, data) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+
+
+app.get('/tinder/cards', (req, res) => {
+  Cards.find().then((error, data) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
